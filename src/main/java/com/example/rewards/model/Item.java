10128b46a586b716e577a5182f9aa6af5b84a53a
Jerum Hubbert <jerum.hubbert@scientificgames.com>
2025-05-25 17:31:49 -0500

    adding more to database

---
+++ src/main/java/com/example/rewards/model/Item.java
@@ -0,0 +1,19 @@
+package com.example.rewards.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "items")
+@Getter
+@Setter
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+}
